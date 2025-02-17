5da43707c24e5436ba84093415f6dc7a1d24c676
Khambrel Simpson <khamsimp18@gmail.com>
2025-02-17 14:27:00 -0800

    added putchar and getchar!

--- day12.c
+++ day12.c
@@ -7,6 +7,14 @@ Line 1 of inputs is how many characters long?
 int main(int argc, char const *argv[]){
   /* The getchar() function can do this. */
   int garden_width = 0;
+  int plant_plot = 0;
+
+  plant_plot = getchar();
+  while (plant_plot != EOF){
+    putchar(plant_plot);
+    plant_plot = getchar();
+    garden_width = garden_width + 1;
+  }
 
   printf("The garden width is: %i\n", garden_width);
 
