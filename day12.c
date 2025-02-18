cf3aa544adb26f79a3dc214a5f57f07f2a20377b
Khambrel Simpson <khamsimp18@gmail.com>
2025-02-18 10:02:28 -0800

    get_height_width function was instantiated and created

--- day12.c
+++ day12.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /* Advent of code day 12 C code */
 
-int main(int argc, char const *argv[]){
-  /* This section of code may become the getwidth getheight function. */
+int get_height_width(void){
+  /* The get_width get_height function. */
   int garden_width = 0;
   int garden_height = 0;
   int plant_plot = 0;
@@ -16,9 +16,15 @@ int main(int argc, char const *argv[]){
 
   garden_height = garden_width;
 
-
   printf("The garden width is: %i\n", garden_width);
   printf("The garden height is: %i\n", garden_height);
+
+  return 0;
+}
+
+int main(int argc, char const *argv[]){
+
+  get_height_width();
 
   plant_plot = 0;
 
