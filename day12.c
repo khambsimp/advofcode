ce7ce5a2cdeb71f137a7f6a5f0c73d37407171ea
Khambrel Simpson <khamsimp18@gmail.com>
2025-02-17 16:32:41 -0800

    Saved width and height of the garden plot.

--- day12.c
+++ day12.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-/* Advent of code day 12 Python Scripts */
-/* Find the length of the row of char inputs.
-Line 1 of inputs is how many characters long?
-*/
+/* Advent of code day 12 C code */
 
 int main(int argc, char const *argv[]){
   /* The getchar() function can do this. */
   int garden_width = 0;
+  int garden_height = 0;
   int plant_plot = 0;
 
   plant_plot = getchar();
+
   while (plant_plot != 10){
     putchar(plant_plot);
     plant_plot = getchar();
     garden_width = garden_width + 1;
   }
 
+  garden_height = garden_width;
+
+
   printf("The garden width is: %i\n", garden_width);
+  printf("The garden height is: %i\n", garden_height);
 
   return 0;
 }
