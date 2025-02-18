4bd9182114ab4ed093debd8a6f08c4c3a47c42a6
Khambrel Simpson <khamsimp18@gmail.com>
2025-02-18 13:39:42 -0800

    seeking a new approach to simplify this bit of code.

--- day12.c
+++ day12.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /* Advent of code day 12 C code */
 
-int get_height_width(void){
-  /* The get_width get_height function. */
+int main(int argc, char const *argv[]){
+
+  /* The get_width get_height potion */
   int garden_width = 0;
   int garden_height = 0;
   int plant_plot = 0;
@@ -18,13 +19,6 @@ int get_height_width(void){
 
   printf("The garden width is: %i\n", garden_width);
   printf("The garden height is: %i\n", garden_height);
-
-  return 0;
-}
-
-int main(int argc, char const *argv[]){
-
-  get_height_width();
 
   int plant_plot = 0;
 
