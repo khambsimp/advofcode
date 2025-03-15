67eddd3d83858ecff3518ccef2a298d455c0b595
Khambrel Simpson <khamsimp18@gmail.com>
2025-03-14 22:58:47 -0700

    new row

--- day12.c
+++ day12.c
@@ -4,26 +4,50 @@
 int main(int argc, char const *argv[]){
 
   /* Set Constants */
-  int garden_width = -1;
+  int garden_width = 0;
   int garden_height = 0;
   int plant_plot = 0;
-  int new_plot_kind = 0;
-  int last_plot_kind = 0;
-  int pot_row = 0;
+  int first_plot_kind = 0;
+  int kind_width = 1;
+  int perimeter = 0;
 
-  while (plant_plot != 10){
+  // First Plot
+  plant_plot = getchar();
+  first_plot_kind = plant_plot;
+
+  // First Row
+  while (plant_plot != 10)
+  {
     plant_plot = getchar();
-    new_plot_kind = plant_plot;
+    if (first_plot_kind == plant_plot)
+    {
+      kind_width = kind_width + 1;
+    }
     garden_width = garden_width + 1;
-
-    /* Print the char of the first plant plot row */
-    printf("%c", new_plot_kind);
   }
+  perimeter = (kind_width * 2) + 2;
+
+  // Second Row
+  plant_plot = 0;
+  kind_width = 1;
+
+  while (plant_plot != 10)
+  {
+    plant_plot = getchar(); // B
+    if (first_plot_kind == plant_plot)
+    {
+      kind_width = kind_width + 1;
+    }
+  }
+  perimeter = (kind_width * 2) + 2;
+
+  garden_height = garden_width;
+
 
   while (plant_plot != EOF){
     plant_plot = getchar();
   }
-  garden_height = garden_width;
+
 
   printf("The garden width is: %i\n", garden_width);
   printf("The garden height is: %i\n", garden_height);
